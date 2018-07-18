5064c1cbfe0ae6d3e568d75f53d7892ddfa917e8
hankunwang <hugowangchn@gmail.com>
2018-07-19 01:01:51 +1200

    test

--- packages/wechat/src/Listeners/TemplateMessageListener.php
+++ packages/wechat/src/Listeners/TemplateMessageListener.php
@@ -30,8 +30,36 @@ class TemplateMessageListener
     {
         $template_name = config('wechat.template_messages')[$event->template_id];
         $template = file_get_contents(storage_path('app/wechat_templates/'.$template_name));
-        Log::info($template);
-        Log::info($event->args);
+        $template = <<<TEMPLETE
+        {
+   "touser":"%s",
+   "template_id":"P8XV__s7izfjopd4sJPMLAB0g-h5nr-X3KPnLHsI8XU",
+   "url":"%s",
+   "data":{
+       "first": {
+           "value":"收到新的留言！",
+           "color":"#173177"
+       },
+       "keyword1":{
+           "value":"Subsriber",
+           "color":"#173177"
+       },
+       "keyword2": {
+           "value":"%s",
+           "color":"#173177"
+       },
+       "keyword3": {
+           "value":"%s",
+           "color":"#173177"
+       },
+       "remark":{
+           "value":"请及时回复！",
+           "color":"#173177"
+       }
+   }
+}  
+TEMPLETE;
+
         $json = printf($template,...$event->args);
         Log::error($json);
         Log::info(Wechat::message()->sendTemplateMessage($json));
